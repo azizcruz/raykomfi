7500aa832d60b035277bf96d85166cbf7223666c
azizcruz <learning094@gmail.com>
2020-11-17 21:00:00 +0100

    fixing static

--- src/settings.py
+++ src/settings.py
@@ -17,7 +17,8 @@ from google.oauth2 import service_account
 from dotenv import load_dotenv
 import json
 load_dotenv()
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
