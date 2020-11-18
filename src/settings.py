d19b96437ad273d2b84875806ada15052f507d0e
azizcruz <learning094@gmail.com>
2020-11-18 20:02:20 +0100

    added site id

--- src/settings.py
+++ src/settings.py
@@ -296,3 +296,5 @@ USER_ONLINE_TIMEOUT = 300
 # Number of seconds that we will keep track of inactive users for before 
 # their last seen is removed from the cache
 USER_LASTSEEN_TIMEOUT = 60 * 60 * 24 * 7
+
+SITE_ID=os.getenv('SITE_ID')
