c526b86c52c80b9a83784ddc84a383a0b0a6fbb1
azizcruz <learning094@gmail.com>
2021-01-03 13:15:30 +0100

    hotfix

--- src/static/js/replacePlainLinks.js
+++ src/static/js/replacePlainLinks.js
@@ -1,8 +1,8 @@
-// function replacePlainLinks(text) {
-//    var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-//    var element_content=text.replace(exp_match, "<a href='$1' target='_blank'>$1</a>");
-//    var new_exp_match =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
-//    var new_content=element_content.replace(new_exp_match, '$1<a target="_blank" href="http://$2">$2</a>');
+function replacePlainLinks(text) {
+   var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+   var element_content=text.replace(exp_match, "<a href='$1' target='_blank'>$1</a>");
+   var new_exp_match =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
+   var new_content=element_content.replace(new_exp_match, '$1<a target="_blank" href="http://$2">$2</a>');
 
-//    return new_content
-// }
+   return new_content
+}
